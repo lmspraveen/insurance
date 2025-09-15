7661080ffd317e18efd884e2968533951d258531
mahendra-lms <mahendra.malviya@lmsin.com>
2025-09-15 11:51:55 +0530

    Design policy request

---
+++ src/app/common/policy-request/approve-request/approve-request.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-approve-request',
+  imports: [],
+  templateUrl: './approve-request.html',
+  styleUrl: './approve-request.scss'
+})
+export class ApproveRequest {
+
+}
---
+++ src/app/common/policy-request/create-policy-request/create-policy-request.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-policy-request',
+  imports: [],
+  templateUrl: './create-policy-request.html',
+  styleUrl: './create-policy-request.scss'
+})
+export class CreatePolicyRequest {
+
+}
