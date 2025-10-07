b1ca8a212b282ff09137ebcbce07e0a4829b563b
mahendra-lms <mahendra.malviya@lmsin.com>
2025-10-07 15:30:49 +0530

    Fix multiple issues

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -43,7 +43,7 @@ export class Login {
 
   openSnackBar(message: string) {
     this._snackBar.open(message, '', {
-      duration: 50000,
+      duration: 5000,
       horizontalPosition: this.horizontalPosition,
       verticalPosition: this.verticalPosition,
       panelClass: ['center-snackbar'],
